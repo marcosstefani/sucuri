53e907eba589459b837715484e37373934a7c603
Marcos Rosa <marcos.cantor@gmail.com>
2018-03-10 07:34:46 -0300

    initial configuration for pyPI Live

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='sucuri',
+    version='0.1.1',
+    url='https://github.com/marcosstefani/sucuri',
+    license='MIT License',
+    author='Marcos Rosa',
+    author_email='marcos.cantor@gmail.com',
+    keywords='template engine python',
+    description=u'Simple and efficient Template Engine for Python projects',
+    packages=['sucuri'],
+    install_requires=[],
+)
