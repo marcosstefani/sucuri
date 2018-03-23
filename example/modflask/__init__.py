49639996674e1bb1445bf561bf6d342c42dcf50f
Marcos Rosa <marcos.cantor@gmail.com>
2018-03-22 21:11:36 -0300

    example of how to use

--- example/modflask/__init__.py
+++ example/modflask/__init__.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    template = rendering.template('template.suc')
+    # template = rendering.template('template.suc')
+    template = rendering.template('template_data.suc',{"a": 1, "b": "Hello!"})
     return render_template_string(template)
