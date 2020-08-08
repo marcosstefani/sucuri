65f38a9ebf4873b37c328b02b23f721927611503
Marcos Stefani <marcos.stefani@m4u.com.br>
2020-08-08 00:24:53 -0300

    remove files to restart

--- example/modflask/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template_string
-from sucuri import rendering
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    template = rendering.template('template.suc',{"text": "Hello! I'm here!", "var":[1, 2, 3, 4]})
-    return render_template_string(template)
--- sucuri/rendering.py
+++
@@ -1,7 +0,0 @@
-from .files import Files
-
-def template(fileName, obj=None):
-    files = Files()
-
-    files.add( fileName )
-    return files.template( fileName, obj )
