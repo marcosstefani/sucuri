c44ce7d60cd833087814c55aa66a916b623dc66d
Marcos Stefani <marcos.stefani@m4u.com.br>
2020-08-08 02:03:27 -0300

    more than one line

--- tests/element/test_text.py
+++ tests/element/test_text.py
@@ -3,6 +3,24 @@ sys.path.append('...')
 
 from sucuri.element.text import Text
 
-def test_should_reflect_when_houver_variaveis_one_line():
+def test_should_reflect_when_there_are_variables_one_line():
     text = Text('Hello {a}', {"a": "World!"})
     assert text.render() == 'Hello World!'
+
+def test_should_reflect_when_there_are_variables_more_than_one_line():
+    old = """Hello!
+    | Text
+    | with
+    | variable {foo} and
+    | more than
+    | one line
+    """
+
+    new = """Hello!
+Text
+with
+variable foo and
+more than
+one line"""
+    text = Text(old, {"foo": "foo"})
+    assert text.render() == new
